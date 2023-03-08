b7e512e47c10b303d1c84e1d567dc9e7e723adef
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-03-08 13:28:07 -0800

    Remove unnecessary divs

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -5,54 +5,52 @@ class Education extends Component {
     render() { 
         const {onChange} = this.props
         return (
-            <div>
-                <form>
-                    <fieldset>
-                        <legend>Education:</legend>
-                        <div>
-                            <label htmlFor="schoolName">Shool Name</label>
-                            <input
-                                type="text"
-                                id="schoolName"
-                                placeholder='School Name'
-                                onChange={(e) => onChange(e, 'school')}
-                            />
-                        </div>
+            <form>
+                <fieldset>
+                    <legend>Education:</legend>
+                    <div>
+                        <label htmlFor="schoolName">Shool Name</label>
+                        <input
+                            type="text"
+                            id="schoolName"
+                            placeholder='School Name'
+                            onChange={(e) => onChange(e, 'school')}
+                        />
+                    </div>
 
-                        <div>
-                            <label htmlFor="degree">Degree:</label>
-                            <input
-                                type="text"
-                                id='degree'
-                                placeholder='Degree'
-                                onChange={(e) => onChange(e, 'degree')}
-                            />
-                        </div>
+                    <div>
+                        <label htmlFor="degree">Degree:</label>
+                        <input
+                            type="text"
+                            id='degree'
+                            placeholder='Degree'
+                            onChange={(e) => onChange(e, 'degree')}
+                        />
+                    </div>
 
-                        <div>
-                            <label htmlFor="from">From:</label>
-                            <input
-                                type="text"
-                                id='from'
-                                placeholder='From'
-                                onChange={(e) => onChange(e, 'from')}
-                            />
-                        </div>
+                    <div>
+                        <label htmlFor="from">From:</label>
+                        <input
+                            type="text"
+                            id='from'
+                            placeholder='From'
+                            onChange={(e) => onChange(e, 'from')}
+                        />
+                    </div>
 
-                        <div>
-                            <label htmlFor="to">To:</label>
-                            <input
-                                type="text"
-                                inputMode='to'
-                                placeholder='To'
-                                onChange={(e) => onChange(e, 'to')}
-                            />
-                        </div>
-                    <button>Delete</button>
-                    <button>Add</button>
-                    </fieldset>
-                </form>
-            </div>
+                    <div>
+                        <label htmlFor="to">To:</label>
+                        <input
+                            type="text"
+                            inputMode='to'
+                            placeholder='To'
+                            onChange={(e) => onChange(e, 'to')}
+                        />
+                    </div>
+                <button>Delete</button>
+                <button>Add</button>
+                </fieldset>
+            </form>
         );
     }
 }
--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -7,64 +7,62 @@ class Personal extends Component {
         const { onChange, onClick } = this.props;
 
         return (
-            <div>
-                <form>
-                    <fieldset>
-                        <legend>Personal Information</legend>
-                        <div>
-                            <label htmlFor="nameInput">Name: </label>
-                            <input
-                                type="text"
-                                id='nameInput'
-                                placeholder='Name'
-                                onChange={(e) => onChange(e, 'name')}
-                            />
-                        </div>
-                        
-                        <div>
-                            <label htmlFor="emailInput">Email: </label>
-                            <input
-                                type="text"
-                                id='emailInput'
-                                placeholder='email'
-                                onChange={(e) => onChange(e, 'email')}
-                            />
-                        </div>
-                        
-                        <div>
-                            <label htmlFor="titleInput">Title:</label>
-                            <input
-                                type="text"
-                                id='titleInput'
-                                placeholder='Title'
-                                onChange={(e) => onChange(e, 'title')}
-                            />
-                        </div>
+            <form>
+                <fieldset>
+                    <legend>Personal Information</legend>
+                    <div>
+                        <label htmlFor="nameInput">Name: </label>
+                        <input
+                            type="text"
+                            id='nameInput'
+                            placeholder='Name'
+                            onChange={(e) => onChange(e, 'name')}
+                        />
+                    </div>
+                    
+                    <div>
+                        <label htmlFor="emailInput">Email: </label>
+                        <input
+                            type="text"
+                            id='emailInput'
+                            placeholder='email'
+                            onChange={(e) => onChange(e, 'email')}
+                        />
+                    </div>
+                    
+                    <div>
+                        <label htmlFor="titleInput">Title:</label>
+                        <input
+                            type="text"
+                            id='titleInput'
+                            placeholder='Title'
+                            onChange={(e) => onChange(e, 'title')}
+                        />
+                    </div>
 
-                        <div>
-                            <label htmlFor="phoneInput">Phone Number:</label>
-                            <input
-                                type="tel"
-                                id='phoneInput'
-                                placeholder='Phone Number'
-                                onChange={(e) => onChange(e, 'phone')}
-                            />
-                        </div>
-                        
-                        <div>
-                            <label htmlFor="descriptionInput">Description:</label>
-                            <textarea
-                                name="description"
-                                id="descriptionInput"
-                                cols="30" rows="10"
-                                onChange={(e) => onChange(e, 'description')}
-                            ></textarea>
-                        </div>
-                    </fieldset>
-                    <button onClick={onClick}>Submit</button>
-                </form>
-            </div>
-        );
+                    <div>
+                        <label htmlFor="phoneInput">Phone Number:</label>
+                        <input
+                            type="tel"
+                            id='phoneInput'
+                            placeholder='Phone Number'
+                            onChange={(e) => onChange(e, 'phone')}
+                        />
+                    </div>
+                    
+                    <div>
+                        <label htmlFor="descriptionInput">Description:</label>
+                        <textarea
+                            name="description"
+                            id="descriptionInput"
+                            cols="30" rows="10"
+                            onChange={(e) => onChange(e, 'description')}
+                        ></textarea>
+                    </div>
+                </fieldset>
+                <button onClick={onClick}>Submit</button>
+            </form>
+    );
     }
 }
  
